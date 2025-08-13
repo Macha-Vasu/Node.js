64589cd77f696405df4a566a8bf761b2b54fa164
Macha-Vasu <macha.vasu@outlook.com>
2025-08-13 11:47:42 +0530

    i have to to change the rep

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,5 @@ const authController = require("../controllers/authController");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-
+console.log("helo");
 module.exports = router;
